88347c6e24371623283349451b17f37eff8934af
dexter <178529795@qq.com>
2022-03-01 20:42:59 +0800

    适配engine变更

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package jessica // import "m7s.live/plugin/jessica/v4"
 
-
 import (
-	. "github.com/logrusorgru/aurora"
-	"go.uber.org/zap"
 	. "m7s.live/engine/v4"
 	"m7s.live/engine/v4/config"
 )
@@ -13,17 +10,7 @@ type JessicaConfig struct {
 	config.Subscribe
 }
 
-var jConfig JessicaConfig
-var plugin = InstallPlugin(&jConfig)
+var plugin = InstallPlugin(new(JessicaConfig))
 
 func (j *JessicaConfig) OnEvent(event any) {
-	switch event.(type) {
-	case FirstConfig:
-		if j.ListenAddr != "" || j.ListenAddrTLS != "" {
-			plugin.Info(Green("Jessica Server Start").String(), zap.String("ListenAddr", j.ListenAddr), zap.String("ListenAddrTLS", j.ListenAddrTLS))
-			go j.Listen(plugin, &jConfig)
-		} else {
-			plugin.Info(Green("Jessica start reuse engine port").String())
-		}
-	}
 }
